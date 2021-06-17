e8251ed9d27a4ef1dbecaaf3ee745a64185edca3
Randy Kipkurui <randy.kipkurui4@gmail.com>
2021-06-17 10:29:14 -0700

    Tasks day 4 comm

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ * Description - Task 4 printing string
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
